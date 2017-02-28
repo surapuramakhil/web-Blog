4a19cd6b52f7858983a6fe84521f4eadbb651679
surapuramakhil <surapuramakhil@gmail.com>
2017-03-01 00:22:36 +0530

    updated schema and package.json

--- schema.sql
+++ schema.sql
@@ -1,97 +1,81 @@
 
-CREATE TABLE article (
-    id integer NOT NULL,
-    title text NOT NULL,
-    heading text NOT NULL,
-    date date NOT NULL,
-    content text NOT NULL
-);
+CREATE SEQUENCE public.article_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
 
+CREATE TABLE public.article
+(
+  id integer NOT NULL DEFAULT nextval('article_id_seq'::regclass),
+  title text NOT NULL,
+  heading text NOT NULL,
+  date date NOT NULL DEFAULT now(),
+  content text NOT NULL,
+  CONSTRAINT article_id PRIMARY KEY (id),
+  CONSTRAINT article_title UNIQUE (title)
+)
 
-CREATE SEQUENCE article_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+CREATE SEQUENCE public.user_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 3
+  CACHE 1;
 
+CREATE TABLE public."user"
+(
+  id integer NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+  username text NOT NULL,
+  password text NOT NULL,
+  age smallint NOT NULL,
+  CONSTRAINT user_id PRIMARY KEY (id),
+  CONSTRAINT user_username UNIQUE (username)
+)
 
-CREATE TABLE comment (
-    id integer NOT NULL,
-    article_id integer NOT NULL,
-    user_id integer NOT NULL,
-    comment text NOT NULL,
-    "timestamp" timestamp with time zone DEFAULT now() NOT NULL
-);
+CREATE SEQUENCE public."label_ID_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 2
+  CACHE 1;
 
+CREATE TABLE public.label
+(
+  "ID" integer NOT NULL DEFAULT nextval('"label_ID_seq"'::regclass),
+  article_id integer NOT NULL,
+  name character varying NOT NULL,
+  CONSTRAINT "label_ID" PRIMARY KEY ("ID"),
+  CONSTRAINT label_article_id_fkey FOREIGN KEY (article_id)
+      REFERENCES public.article (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT label_name_article_id UNIQUE (name, article_id)
+)
 
+CREATE SEQUENCE public.comment_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
 
-CREATE SEQUENCE comment_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-
-CREATE TABLE "user" (
-    id integer NOT NULL,
-    username text NOT NULL,
-    password text NOT NULL
-);
-
-
-
-CREATE SEQUENCE user_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-
-ALTER TABLE ONLY article ALTER COLUMN id SET DEFAULT nextval('article_id_seq'::regclass);
-
-
-
-ALTER TABLE ONLY comment ALTER COLUMN id SET DEFAULT nextval('comment_id_seq'::regclass);
-
-
-
-ALTER TABLE ONLY "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regclass);
-
-
-ALTER TABLE ONLY article
-    ADD CONSTRAINT article_id PRIMARY KEY (id);
-
-
-
-ALTER TABLE ONLY article
-    ADD CONSTRAINT article_title UNIQUE (title);
-
-
-
-ALTER TABLE ONLY comment
-    ADD CONSTRAINT comment_id PRIMARY KEY (id);
-
-
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_id PRIMARY KEY (id);
-
-
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_username UNIQUE (username);
-
-
-
-ALTER TABLE ONLY comment
-    ADD CONSTRAINT comment_article_id_fkey FOREIGN KEY (article_id) REFERENCES article(id);
-
-
-
-ALTER TABLE ONLY comment
-    ADD CONSTRAINT comment_user_id_fkey FOREIGN KEY (user_id) REFERENCES "user"(id);
+CREATE TABLE public.comment
+(
+  id integer NOT NULL DEFAULT nextval('comment_id_seq'::regclass),
+  article_id integer,
+  user_id integer NOT NULL,
+  comment text NOT NULL,
+  "timestamp" timestamp with time zone NOT NULL DEFAULT now(),
+  comment_id integer,
+  CONSTRAINT comment_id PRIMARY KEY (id),
+  CONSTRAINT comment_article_id_fkey FOREIGN KEY (article_id)
+      REFERENCES public.article (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT comment_comment_id_fkey FOREIGN KEY (comment_id)
+      REFERENCES public.comment (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT comment_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES public."user" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
