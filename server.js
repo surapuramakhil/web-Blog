1ca3f3815d3233df04661beb682a36f8477c70db
surapuramakhil <surapuramakhil@gmail.com>
2017-03-16 22:20:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -178,7 +178,7 @@ app.get('/fonts/glyphicons-halflings-regular.ttf', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/fonts', 'glyphicons-halflings-regular.ttf'));
 });
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+var port = 80; // Use 8080 for local development because you might already have apache running on 80
+app.listen(80, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
